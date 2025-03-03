39389c2449f2e45962fd74a57a4298d3b0f67ef5
巫汉良 <804879175@qq.com>
2025-03-03 09:30:15 +0800

    初始化项目

---
+++ CopyDouYuApp/CopyDouYuAppTests/CopyDouYuAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  CopyDouYuAppTests.swift
+//  CopyDouYuAppTests
+//
+//  Created by 巫汉良 on 2025/3/2.
+//
+
+import Testing
+@testable import CopyDouYuApp
+
+struct CopyDouYuAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
